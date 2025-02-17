daba64e9359abd96df2148b9859fb9c29a6f7f5b
SenaKebe <152234422+SenaKebe@users.noreply.github.com>
2025-02-17 09:27:31 +0300

    nav-bar partially completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,12 +31,12 @@ const Navbar = () => {
             toggle ? "flex" : "hidden"
           } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
         >
-          <ul className="list-none flex  jusify-end items-center flex-1 ">
+          <ul className="list-none flex flex-col jusify-end items-center flex-1 ">
             {navLinks.map((nav, index) => (
               <li
                 key={nav.id}
                 className={`font-poppins font-normal cursor-pointer text-[16px] text-white             
-              ${index === navLinks.length - 1 ? "mr-0" : "mr-10"} `}
+              ${index === navLinks.length - 1 ? "mr-0" : "mb-4"} `}
               >
                 <a href={`#${nav.id}`}>{nav.title}</a>
               </li>
